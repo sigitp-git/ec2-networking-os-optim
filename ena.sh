2e6798a0583498b8814ededade61c25cabf31194
sigitp-git <sigitp@amazon.com>
2020-12-07 09:48:50 -0600

    ena update dkms

--- ena.sh
+++ ena.sh
@@ -55,3 +55,4 @@ sudo apt-get install -y build-essential dkms
 git clone https://github.com/amzn/amzn-drivers
 sudo mv amzn-drivers /usr/src/amzn-drivers-1.0.0
 sudo touch /usr/src/amzn-drivers-1.0.0/dkms.conf
+sudo vim /usr/src/amzn-drivers-1.0.0/dkms.conf
